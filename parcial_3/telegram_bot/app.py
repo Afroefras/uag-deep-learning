3fd0a66df001a91f639d35a1e25426e93e20239a
EF <efraisma.ef7@gmail.com>
2026-04-25 14:03:21 -0600

    Update app.py

--- parcial_3/telegram_bot/app.py
+++ parcial_3/telegram_bot/app.py
@@ -31,7 +31,7 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     try:
         # 1. Crear directorio si no existe
-        folder = Path("input_photos")
+        folder = Path("inputs")
         folder.mkdir(exist_ok=True)
 
         # 2. Generar nombre dinámico: ID_YYYYMMDD_HHMMSS.jpg
